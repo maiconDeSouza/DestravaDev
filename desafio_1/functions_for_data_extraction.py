ee08b34aca4ce23b6bd180c5d3a2113d71c9315e
Maicon Souza <maicon_souza09@hotmail.com>
2024-08-06 18:29:45 -0300

    06-08-24 18:29

--- desafio_1/functions_for_data_extraction.py
+++ desafio_1/functions_for_data_extraction.py
@@ -9,11 +9,23 @@ service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
 def navigate_to_the_website(url):
-    driver.get(url)
+    try:
+        driver.get(url)
+    except Exception as e:
+        print(e)
+        print("Algo de errado ocorreu ao acessar o navegador!")
 
 def extract_data(data_label, xpath):
-    element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, xpath)))
-    return {data_label: element.text}
+    try:
+        element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, xpath)))
+        return {data_label: element.text}
+    except Exception as e:
+        print(e)
+        print("Algum erro ao pegar o valor dos elementos!")
 
 def quit_website():
-    driver.quit()
+    try:
+        driver.quit()
+    except Exception as e:
+        print(e)
+        print("Erro ao tentar fechar o navegador!")
